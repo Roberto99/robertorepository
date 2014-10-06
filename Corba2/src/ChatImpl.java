2c342b8fed98bcd4dc0b89c309487491f6f7977a
Zoubir <Zoubir@ZL>
2014-10-06 14:07:50 +0200

    Version0

---
+++ Corba2/src/ChatImpl.java
@@ -0,0 +1,34 @@
+
+public class ChatImpl extends ChatPOA {
+
+	@Override
+	public void connect(String pseudo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void disconnect(String pseudo) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String[] getClients() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void sendMessage(String from, String to, String message) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getMessage(String pseudo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ Corba2/src/Client.java
@@ -0,0 +1,103 @@
+import org.omg.CORBA.*;
+import org.omg.CosNaming.*;
+import org.omg.CORBA.ORBPackage.InvalidName;
+
+
+
+public class  Client{
+
+public static void main(String args[])
+{
+    java.util.Properties props = System.getProperties();
+    
+
+    int status = 0;
+    org.omg.CORBA.ORB orb = null;
+
+try
+    {
+    orb = ORB.init(args, props);
+    run(orb);
+    }
+catch(Exception ex)
+    {
+    ex.printStackTrace();
+    status = 1;
+    }
+
+if(orb != null)
+{
+    try
+    {
+    orb.destroy();
+    }
+    catch(Exception ex)
+    {
+    ex.printStackTrace();
+    status = 1;
+    }
+}
+
+System.exit(status);
+}
+
+static void run(ORB orb)
+{
+    org.omg.CORBA.Object obj = null;
+	
+	try
+	{
+	
+	obj=orb.resolve_initial_references("NameService");
+	}
+	catch(InvalidName e)
+	{
+		e.printStackTrace();
+		System.exit(1);
+	}
+	
+	
+	NamingContext ctx = NamingContextHelper.narrow(obj);
+	
+	
+	if (ctx==null)
+	{
+		System.out.println("Le composant NameService n'est pas un repertoire");
+		System.exit(1);
+	}
+	
+	NameComponent[] name = new NameComponent[1];
+	
+	name[0]=new NameComponent("ChatService","");
+	
+	try
+	{
+	obj = ctx.resolve(name);
+	}
+	catch (Exception e)
+	{
+		System.out.println("Composant inconnu");
+		e.printStackTrace();
+		System.exit(1);
+	}
+	
+  /*
+    String refFile = "Hello.ref";
+    java.io.BufferedReader in = new java.io.BufferedReader(
+    new java.io.FileReader(refFile));
+    String ref = in.readLine();
+    System.out.println("IOR :"+ref);
+    obj = orb.string_to_object(ref);
+    //obj = orb.string_to_object("relfile:/Hello.ref");*/
+
+Chat chat = ChatHelper.narrow(obj);
+
+chat.connect("Moi");
+//System.out.println(chat.connect("Moi"));
+
+
+}
+
+
+}
+
